ee96e709153eeb31351c410aca53c39f9eeaaadf
tigerater <tigerater@gmail.com>
2018-05-27 03:53:52 +0100

    Made counter and capitalisation also made it not recognise quotes
    Thanks to u/AutisticJewLizard for the quote detection, u/jmc323 for capitalisation and u/GilArcher for the counting! Your ideas are appreciated!

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ import praw
 import pdb
 import re
 import os
+from num2words import num2words
 
 reddit = praw.Reddit('bot1')
 
 def checkcondition(c):
     text = c.body
-    if "spiderman" in text or "Spiderman" in text:
-        return True
-    else:
-        return False
-    
-#subreddit = reddit.get_subreddit('spiderbot')
-#comments = subreddit.get_comments(limit=100)
+    if text[0] != '>':
+        if "spiderman" in text or "Spiderman" in text:
+            return True
+        else:
+            return False
+
 comments = reddit.subreddit('marvelstudios').comments(limit=500)
 
 if not os.path.isfile("comments_replied_to.txt"):
@@ -26,11 +26,19 @@ else:
        comments_replied_to = f.read()
        comments_replied_to = comments_replied_to.split("\n")
        comments_replied_to = list(filter(None, comments_replied_to))
+
+def checkcounter():
+    with open("counter.txt", "r") as f:
+        counter = int(f.read())
+        return(num2words(counter, to='ordinal'))
+        counter += 1
+    with open("counter.txt", "w") as g:
+        g.write(str(counter))
        
 for c in comments:
     if c.id not in comments_replied_to:
         if checkcondition(c):
-            c.reply("Mr. Stark come on, it's Spider-man, this must have been the millionth time by now :/ \n\n I'm a bot created by u/tigerater to correct the lack of hyphens found in Spider-man! I'll only be up in for a few days but please pm my owner if you have any questions!")
+            c.reply("Mr. Stark come on, it's Spider-Man, this must have been the " + str(checkcounter()) + " time by now :/ \n\n I'm a bot created by u/tigerater to correct the lack of hyphens found in Spider-Man! I'll only be up in for a few days but please pm my owner if you have any questions!")
             comments_replied_to.append(c.id)
     with open("comments_replied_to.txt", "w") as f:
         for comment_id in comments_replied_to:
